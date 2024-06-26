5967646900a2f1781a18a0a87e2dcb5bfe6e2d56
LeGia179 <Le_g_minh@outlook.de>
2024-06-26 14:00:09 +0200

    Websocket konfiguriert test 08

--- frontend/src/components/MessageListComponent.vue
+++ frontend/src/components/MessageListComponent.vue
@@ -69,7 +69,7 @@ onUnmounted(() => {
 });
 
 function initWebSocket() {
-  ws = new WebSocket('wss:https://moglichat-odov.onrender.com/ws');
+  ws = new WebSocket('wss://moglichat-odov.onrender.com/ws');
 
   ws.onopen = () => {
     console.log('WebSocket connection established');
