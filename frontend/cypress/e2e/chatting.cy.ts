933a281537ad799cb9b2b9c40c0a56190df4249b
Phuc Hong Le Ehlert <phuchongle.ehlert@gmail.com>
2024-06-27 00:03:45 +0200

    action test

---
+++ frontend/cypress/e2e/chatting.cy.ts
@@ -0,0 +1,14 @@
+/*describe('template spec', () => {
+  it('passes', () => {
+    cy.viewport(1920,1080)
+    cy.visit('http://localhost:5173/')
+    cy.get('.loginButton').click();
+    cy.get('.emailbox').type("test");
+    cy.get('.passwordbox').type("test");
+    cy.get('.loginButton').click();
+
+    cy.get('.inputField').type("ich bin ein test");
+    cy.get('.sendButton').click();
+  })
+})
+*/
