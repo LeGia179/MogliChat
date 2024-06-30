218f2ce246ee5e378feb7e9ff8c1a2c365bfae80
LeGia179 <Le_g_minh@outlook.de>
2024-06-30 23:12:07 +0200

    cypress test02

--- frontend/cypress/e2e/register.cy.ts
+++ frontend/cypress/e2e/register.cy.ts
@@ -3,9 +3,9 @@ describe('template spec', () => {
         cy.viewport(1920,1080)
         cy.visit('http://localhost:5173/')
         cy.get('.register-button').click();
-        cy.get('.username-box').type('test');
-        cy.get('.email-box').type('test@gmail.com');
-        cy.get('.password-box').type('test');
+        cy.get('.username-box').type('test2');
+        cy.get('.email-box').type('test2@gmail.com');
+        cy.get('.password-box').type('test2');
         cy.get('.register-button').click();
 
     })
