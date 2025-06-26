384f96c5464edd9990ac31fe708b7abc2381cef7
LeGia179 <Le_g_minh@outlook.de>
2025-06-26 23:33:08 +0200

    Update 26.06.2025

---
+++ backend/src/main/java/htwberlin/backend/WebSocketTransfer/ChatTransfer.java
@@ -0,0 +1,35 @@
+package htwberlin.backend.WebSocketTransfer;
+
+import lombok.Setter;
+import lombok.Getter;
+
+
+public class ChatTransfer {
+    private String content;
+    private String date;
+    private String sender;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
